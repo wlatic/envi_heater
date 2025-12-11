ee5ad8a8f4c023fa26fe7fe4d7348e3dd0e4bf91
wlatic <jamesoh@gmail.com>
2025-12-10 23:30:01 -0800

    Update __init__.py

--- custom_components/envi_heater/__init__.py
+++ custom_components/envi_heater/__init__.py
@@ -6,11 +6,11 @@ import logging
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from homeassistant.exceptions import ConfigEntryAuthFailed, ConfigEntryNotReady
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
-from .const import DOMAIN
 from .api import EnviApiClient, EnviAuthenticationError
+from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -18,14 +18,9 @@ PLATFORMS = [Platform.CLIMATE]
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Set up Envi Heater from config entry."""
+    """Set up Envi Heater from a config entry."""
     session = async_get_clientsession(hass)
-
-    client = EnviApiClient(
-        session=session,
-        username=entry.data["username"],
-        password=entry.data["password"],
-    )
+    client = EnviApiClient(session, entry.data["username"], entry.data["password"])
 
     try:
         await client.authenticate()
@@ -34,20 +29,16 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     except Exception as err:
         raise ConfigEntryNotReady from err
 
-    hass.data.setdefault(DOMAIN, {})
-    hass.data[DOMAIN][entry.entry_id] = client
-
+    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = client
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
 
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+async def async_unload_entry(hass: HomeAssistant: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
     unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
-
     if unload_ok:
         hass.data[DOMAIN].pop(entry.entry_id, None)
         if not hass.data[DOMAIN]:
-            hass.data.pop(DOMAIN)
-
+            hass.data.pop(DOMAIN, None)
     return unload_ok
